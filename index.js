b5197199e88b132542430b2ad7680361130c0024
TZ <atian25@qq.com>
2015-06-05 11:55:09 +0800

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+var generator = require('generator-install');
+
+module.exports = {
+  name: 'init2',
+  desc: 'init project boilerplate',
+  register: function (commander) {
+    commander
+        .option('-f, --file <filename>', 'generator config remote url or local path')
+        .option('-k, --keyword <keyword>', 'keyword to filter generators')
+        .option('-c, --clean', 'clean generator cache', Boolean, false)
+        .action(function () {
+          var args = Array.prototype.slice.call(arguments);
+          var options = args.pop();
+
+          generator.run({
+            tmpDir: fis.project.getTempPath('generator'),
+            buildin: true,
+            cfg: options.file,
+            pkg: args.shift(),
+            clean: options.clean
+          })
+        });
+  }
+};
